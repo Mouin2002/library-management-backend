06a518fcd898c4412da08f47161c1deebf337c3f
Moulali Shaik <shaikmouin2002@gmail.com>
2026-07-31 23:15:13 +0530

    implement custom user model and manager

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -10,3 +10,43 @@ class RegisterSerializer(serializers.Serializer):
     password = serializers.CharField(write_only=True)
     confirm_password = serializers.CharField(write_only=True)
     role = serializers.ChoiceField(choices=User.Role.choices)
+
+def validate_email(self, value):
+
+    if User.objects.filter(email=value).exists():
+        raise serializers.ValidationError(
+            "Email already registered."
+        )
+
+    return value
+def validate_phone_number(self, value):
+
+    if len(value) != 10:
+        raise serializers.ValidationError(
+            "Phone number must contain 10 digits."
+        )
+
+    return value
+def validate(self, attrs):
+
+    if attrs["password"] != attrs["confirm_password"]:
+        raise serializers.ValidationError(
+            {
+                "confirm_password":
+                "Passwords do not match."
+            }
+        )
+
+    return attrs
+def create(self, validated_data):
+
+    validated_data.pop("confirm_password")
+
+    password = validated_data.pop("password")
+
+    user = User.objects.create_user(
+        password=password,
+        **validated_data
+    )
+
+    return user
